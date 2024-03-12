ce70a32250c2378532d8b9ad656c3f222a991959
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-03-12 09:14:00 -0300

    Alteracao na linha 26, add console

--- jogo_adv_2002.js
+++ jogo_adv_2002.js
@@ -21,5 +21,8 @@ while (numeroPessoa != numeroAleatorio) {
 console.log("Você acertou!! e o numero correto é " + numeroPessoa);
 console.log("Numero de tentativas " + tentativas);
 
-console.log("Ignorar, alteração1")
+console.log("Ignorar, alteração1");
+console.log("Ignorar, alteração2");
+console.log("Ignorar, alteração3");
+
 
